c6fc9fb7d9264d0b3edac56f5b11ffefb3a66c4d
Mason Scarbro <120221711+MasonScarbro@users.noreply.github.com>
2025-04-26 20:40:06 -0400

    what is going on dawg

--- app.py
+++ app.py
@@ -454,14 +454,7 @@ def search_users():
         logging.error(f"Error in /search_users: {str(e)}")
         return jsonify({"error": str(e)}), 500
     
-@app.route('/logout_user', methods=['GET'])
-def current_user():
-    if 'user' in session:
-        user = session['user']
-        session.pop('user', None)
-    if user:
-        return jsonify({"user": user})
-    return jsonify({"error": "No user logged in"}), 401
+
 
 if __name__ == '__main__':
     app.run(debug=True)
